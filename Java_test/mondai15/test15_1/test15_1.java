4097ffb87fec8bd91ea1887a36341fbcc255a800
Frisk9673 <ktq2390301@stu.o-hara.ac.jp>
2024-01-26 14:31:06 +0900

    Commit

--- Java_test/mondai15/test15_1/test15_1.java
+++ Java_test/mondai15/test15_1/test15_1.java
@@ -7,18 +7,17 @@ class test15_1 {
         
         try {
             //整数1の入力
-        System.out.print("整数1 = "); int num1 = stdIn.nextInt();
+            System.out.print("整数1 = "); int num1 = stdIn.nextInt();
 
-        //整数2の入力
-        System.out.print("整数2 = ");
-        int num2 = stdIn.nextInt();
+            //整数2の入力
+            System.out.print("整数2 = ");
+            int num2 = stdIn.nextInt();
 
-        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-        } catch(ArithmeticException e) {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+            } catch(ArithmeticException e) {
             System.out.println("0による割り算です！！");
-        } finally {
+            } finally {
             System.out.println("処理終了");
         }
     }
 }
-
